173d8864fabbeb7afdfc4ee08a43860f5ffb06a6
infinitecoin <63966367+infinitecoin-project@users.noreply.github.com>
2025-06-30 11:55:28 +0800

    New consensus! Fixing the network magic number.
    The original developer of Ininitecoin used the same network magic number as Litecoin, which caused node mixing and cross-connection.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,10 +107,16 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+	    
+	// Update to the new network magic number based on the timestamp
+	// The original developer of Ininitecoin used the same network magic number as Litecoin, which caused node mixing and cross-connection.
+        // 2025-07-07 12:00:00  UTC
+        if (time(NULL) >= 1751889600) {   
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0xbf;
+        pchMessageStart[2] = 0xc0;
+        pchMessageStart[3] = 0xde;
+	}
         nDefaultPort = 9321;
         nPruneAfterHeight = 100000;
 
